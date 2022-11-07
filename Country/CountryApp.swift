603381110f7dd7f6f00a3231ff7aec7142591e78
Shalom <shalom.owolabi@etranzactglobal.com>
2022-11-07 07:16:21 +0100

    Initial Commit

---
+++ Country/CountryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryApp.swift
+//  Country
+//
+//  Created by eTranzact Global Mac 2 on 07/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CountryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
